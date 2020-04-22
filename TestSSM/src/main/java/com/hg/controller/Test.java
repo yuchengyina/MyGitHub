d3aac86ef825effa0207c17df607e48d36003120
qq785262971 <785262971@qq.com>
2020-04-22 09:11:47 +0800

    上传

---
+++ TestSSM/src/main/java/com/hg/controller/Test.java
@@ -0,0 +1,16 @@
+package com.hg.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/test")
+public class Test {
+
+    @RequestMapping("/c")
+    public String s(){
+        return "index";
+    }
+
+}
